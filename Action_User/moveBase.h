0be53526b254c171fc97237cd27c22ab3efe60e9
Meng2222 <Dreamer_Meng2@163.com>
2018-08-09 23:28:22 +0800

    Upload files

---
+++ Action_User/moveBase.h
@@ -0,0 +1,75 @@
+/**
+  ******************************************************************************
+  * @file    .h
+  * @author  ACTION_2017
+  * @version V0.0.0._alpha
+  * @date    2017//
+  * @brief   This file contains all the functions prototypes for 
+  *          
+  ******************************************************************************
+  * @attention
+  *
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MOVEBASE_H
+#define __MOVEBASE_H
+
+
+
+/* Includes ------------------------------------------------------------------*/
+
+
+
+/* Exported types ------------------------------------------------------------*/
+
+/** 
+  * @brief  
+  */
+
+
+ 
+/* Exported constants --------------------------------------------------------*/
+
+
+
+/** @defgroup 
+  * @{
+  */
+
+//#define 
+
+//电机旋转一周的脉冲数
+#define COUNTS_PER_ROUND (4096)
+//轮子直径（单位：mm）
+#define WHEEL_DIAMETER (106.8f)
+//调试小车车长（单位：mm）
+#define MOVEBASE_LENGTH (500.0f)
+//调试小车车宽(单位：mm)
+#define MOVEBASE_WIDTH (403.0f)
+//轮子宽度（单位：mm）
+#define WHEEL_WIDTH (46.0f)
+//两个轮子中心距离（单位：mm）
+#define WHEEL_TREAD (355.4f)
+
+
+/**
+  * @}
+  */
+
+
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+
+
+
+
+#endif /* ___H */
+
+
+
+/************************ (C) COPYRIGHT NEU_ACTION_2017 *****END OF FILE****/
+
---
+++ Action_User/task.c
@@ -0,0 +1,70 @@
+#include "includes.h"
+#include <app_cfg.h>
+#include "misc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "timer.h"
+#include "gpio.h"
+#include "usart.h"
+#include "can.h"
+#include "elmo.h"
+#include "stm32f4xx_it.h"
+#include "stm32f4xx_usart.h"
+
+/*
+===============================================================
+						信号量定义
+===============================================================
+*/
+OS_EXT INT8U OSCPUUsage;
+OS_EVENT *PeriodSem;
+
+static OS_STK App_ConfigStk[Config_TASK_START_STK_SIZE];
+static OS_STK WalkTaskStk[Walk_TASK_STK_SIZE];
+
+void App_Task()
+{
+	CPU_INT08U os_err;
+	os_err = os_err; /*防止警告...*/
+
+	/*创建信号量*/
+	PeriodSem = OSSemCreate(0);
+
+	/*创建任务*/
+	os_err = OSTaskCreate((void (*)(void *))ConfigTask, /*初始化任务*/
+						  (void *)0,
+						  (OS_STK *)&App_ConfigStk[Config_TASK_START_STK_SIZE - 1],
+						  (INT8U)Config_TASK_START_PRIO);
+
+	os_err = OSTaskCreate((void (*)(void *))WalkTask,
+						  (void *)0,
+						  (OS_STK *)&WalkTaskStk[Walk_TASK_STK_SIZE - 1],
+						  (INT8U)Walk_TASK_PRIO);
+}
+
+/*
+   ===============================================================
+   初始化任务
+   ===============================================================
+   */
+void ConfigTask(void)
+{
+	CPU_INT08U os_err;
+	os_err = os_err;
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	OSTaskSuspend(OS_PRIO_SELF);
+}
+
+void WalkTask(void)
+{
+
+	CPU_INT08U os_err;
+	os_err = os_err;
+
+	OSSemSet(PeriodSem, 0, &os_err);
+	while (1)
+	{
+		OSSemPend(PeriodSem, 0, &os_err);
+	}
+}
