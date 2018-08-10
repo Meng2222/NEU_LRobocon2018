df12fec32a889ac00593bf503b1e06de9d1fd4f4
asliu619 <100532929@qq.com>
2018-08-10 22:16:27 +0800

    mission1 openloop

--- Action_User/task.c
+++ Action_User/task.c
@@ -10,7 +10,7 @@
 #include "elmo.h"
 #include "stm32f4xx_it.h"
 #include "stm32f4xx_usart.h"
-
+ #include "moveBase.h"
 /*
 ===============================================================
 						信号量定义
@@ -18,7 +18,8 @@
 */
 OS_EXT INT8U OSCPUUsage;
 OS_EVENT *PeriodSem;
-
+void WalkLine(float speed);
+void WalkRound(float vel,float radius,char side);
 static OS_STK App_ConfigStk[Config_TASK_START_STK_SIZE];
 static OS_STK WalkTaskStk[Walk_TASK_STK_SIZE];
 
@@ -52,7 +53,14 @@ void ConfigTask(void)
 	CPU_INT08U os_err;
 	os_err = os_err;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	
+	TIM_Init(TIM2, 1000-1, 84-1, 0x01, 0x03); //定时器初始化1ms
+	CAN_Config(CAN1,500,GPIOB,GPIO_Pin_8,GPIO_Pin_9);
+	CAN_Config(CAN2,500,GPIOB,GPIO_Pin_5,GPIO_Pin_6);
+	ElmoInit(CAN2);
+	VelLoopCfg(CAN2, 1, 50000, 50000);
+	VelLoopCfg(CAN2, 2, 50000, 50000);
+	MotorOn(CAN2, 1);
+	MotorOn(CAN2, 2);
 	OSTaskSuspend(OS_PRIO_SELF);
 }
 
@@ -66,5 +74,46 @@ void WalkTask(void)
 	while (1)
 	{
 		OSSemPend(PeriodSem, 0, &os_err);
+		//WalkLine(400);
+		WalkRound(1000,1000,1);
 	}
 }
+
+/**
+* @brief  直线函数
+* @param  vel: 速度，单位：mm每秒，范围：最小速度限制到最大速度限制
+*/
+void WalkLine(float vel)
+{
+	float phase = 0;
+	phase = 4096.f * vel / (WHEEL_DIAMETER * PI);
+	VelCrl(CAN2,1,phase);
+	VelCrl(CAN2,2,-phase);
+}
+
+/**
+* @brief  圆形函数
+* @param  vel: 速度，单位：mm每秒，范围：最小速度限制到最大速度限制
+* @param  radius: 半径，单位：mm，范围：最小限制到最大限制
+* @param  side: 半径，1圆心在右，0 圆心正左
+*/
+void WalkRound(float vel,float radius,char side)
+{
+	float phase1 = 0,phase2 = 0;
+	float v1 = 0,v2 = 0;
+	v1 = (radius - WHEEL_TREAD / 2) / radius * vel;
+	v2 = (radius + WHEEL_TREAD / 2) / radius * vel;
+	phase1 = 4096.f *v1 / (WHEEL_DIAMETER * PI);
+	phase2 = 4096.f *v2 / (WHEEL_DIAMETER * PI);
+	if(side == 1)
+	{
+		VelCrl(CAN2,1,phase1);
+		VelCrl(CAN2,2,-phase2);
+	}
+	else if(side == 0)
+	{
+		VelCrl(CAN2,1,phase2);
+		VelCrl(CAN2,2,-phase1);
+	}
+}
+
