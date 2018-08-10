7d97b27ef6b620f48035a0f5316b366fcdc8350a
Pipibin <2920037899@qq.com>
2018-08-10 21:27:18 +0800

    run
    go Straight，left turn，right turn

--- Action_User/moveBase.c
+++ Action_User/moveBase.c
@@ -12,7 +12,9 @@
   */
 /* Includes -------------------------------------------------------------------------------------------*/
 
-  #include "moveBase.h"
+#include "moveBase.h"
+
+#include "elmo.h"
 
 /* Private typedef ------------------------------------------------------------------------------------*/
 /* Private define -------------------------------------------------------------------------------------*/
@@ -22,12 +24,56 @@
 /* Private functions ----------------------------------------------------------------------------------*/
 
 /**
-  * @brief  
-  * @note
-  * @param  
+  * @brief  走直线
+  * @note	
+  * @param  speed：给定速度
   * @retval None
   */
 
+void Straight(float speed)
+{
+	int32_t pulseNum=(speed*4095*1000)/(PI*WHEEL_DIAMETER);
+	
+	VelCrl(CAN2, 0x01,pulseNum);
+	VelCrl(CAN2, 0x02,-pulseNum);
+	
+		
+}
 
+/**
+  * @brief  转圈
+  * @note	
+* @param  rightSpeed：给定右轮速度,正数为逆时针转,为外侧速度；负数顺时针转，为内侧速度
+  * @param  radius：给定半径
+  * @retval None
+  */
+
+void Round(float rightSpeed,float radius)
+{
+	float leftSpeed;
+	int32_t rightPulseNum;
+	int32_t leftPulseNum;
+	//逆时针转
+	if(rightSpeed > 0.0)
+	{
+		rightPulseNum=(rightSpeed*4095*1000)/(PI*WHEEL_DIAMETER);
+		leftSpeed=rightSpeed*(radius-(WHEEL_TREAD/1000))/(radius+(WHEEL_TREAD/1000));
+		leftPulseNum=-(leftSpeed*4095*1000)/(PI*WHEEL_DIAMETER);
+		
+		VelCrl(CAN2, 0x01,rightPulseNum);
+		VelCrl(CAN2, 0x02,leftPulseNum);
+		
+	}
+	//顺时针转
+	else
+	{
+		rightPulseNum=-(rightSpeed*4095*1000)/(PI*WHEEL_DIAMETER);
+		leftSpeed=rightSpeed*(radius+(WHEEL_TREAD/1000))/(radius-(WHEEL_TREAD/1000));
+		leftPulseNum=(leftSpeed*4095*1000)/(PI*WHEEL_DIAMETER);
+		
+		VelCrl(CAN2, 0x01,rightPulseNum);
+		VelCrl(CAN2, 0x02,leftPulseNum);
+	}
+}
 
 /********************* (C) COPYRIGHT NEU_ACTION_2018 ****************END OF FILE************************/
