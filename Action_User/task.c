34e47bc0ceda6c2bd693a84549e7a958ddc551a0
Nathaniel <1016847444@qq.com>
2018-08-21 16:09:10 +0800

    走行任务

--- Action_User/task.c
+++ Action_User/task.c
@@ -17,13 +17,13 @@
 #define Kp1 (98)   //角度PID参数
 #define Ki1 (0)
 #define Kd1 (0) 
-#define Kp2 500.0/800*7 //位置PID参数
+#define Kp2 500.0/1000*7 //位置PID参数
 #define Ki2 (0)
 #define Kd2 (0)
-#define speed (800)  //小车速度
+#define speed (1000)  //小车速度
 #define pointX  (0.0f)  //pointX，pointY：给定圆形的坐标
-#define pointY  (2250.0f)
-float nowAngle,R=2250;
+#define pointY  (2200.0f)
+float nowAngle=90,R=2000,motorState;
 float u1,u2;
 /*
 ===============================================================
@@ -75,12 +75,13 @@ void ConfigTask(void)
 	os_err = os_err;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	
-	CAN_Config(CAN1,500,GPIOB,GPIO_Pin_8,GPIO_Pin_9);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1, ENABLE);
 	CAN_Config(CAN2,500,GPIOB,GPIO_Pin_5,GPIO_Pin_6);
 	
 	TIM_Init(TIM2,999,83,0,0);
 	
 	USART3_Init(115200);   //定位器串口
+	delay_ms(2000);
 	/*一直等待定位系统初始化完成*/
 	WaitOpsPrepare();
 	UART4_Init(921600);    //蓝牙串口
@@ -97,9 +98,9 @@ void uAng()
 	static float lastErr = 0;
 	static float sumErr = 0;
 	float err,pointAngle;
-	if(R>0)
+	if(motorState==0)
 		pointAngle=atan2(pointX-GetX(),GetY()-pointY)*180/PI;
-	if(R<0)
+	if(motorState==1)
 		pointAngle=atan2(GetX()-pointX,pointY-GetY())*180/PI;
 	nowAngle=GetAngle()+90;
 	if(nowAngle>180)
@@ -124,7 +125,7 @@ void uPlace()
 	float err,distance;
     distance=sqrt((pointX-GetX())*(pointX-GetX())+(pointY-GetY())*(pointY-GetY()));
 	err=R-distance;
-	if(R<0)
+	if(motorState==1)
 		err=-err;
 	sumErr += err;
 	u2= Kp2*0.01*err + Ki2 * sumErr +Kd2 *(err - lastErr);
@@ -136,114 +137,104 @@ void uPlace()
 }
 void WalkTask(void)
 {
-	float buff1,buff2,pulse,startState,distance1,distance2;
-	static float lastX=-1,lastY=-1,errState;
+	float buff1,buff2,pulse,distance1,distance2,rState,startState=0;
+	float lastX,lastY,errState=0,errCount,buffstate,buffR;
+	int switchState=0;
 	CPU_INT08U os_err;
 	os_err = os_err;
 	OSSemSet(PeriodSem, 0, &os_err);
-	float state;
 	while (1)
 	{
 		OSSemPend(PeriodSem, 0, &os_err);
 		distance1=Get_Adc_Average(14,10)*(4400/4096);			//设置ADC1通道14,15，平均值获取次数为10
-		distance2=Get_Adc_Average(15,10)*(4400/4096);
+		distance2=Get_Adc_Average(15,10)*(4400/4096);	
 		USART_OUT(UART4,(uint8_t*)"%d\t",(int)nowAngle);
 		USART_OUT(UART4,(uint8_t*)"%d\t",(int)GetX());
 		USART_OUT(UART4,(uint8_t*)"%d\t",(int)GetY());
 		USART_OUT(UART4,(uint8_t*)"%d\t",(int)R);
 		USART_OUT(UART4,(uint8_t*)"%d\t",(int)distance1);
 		USART_OUT(UART4,(uint8_t*)"%d\t",(int)distance2);
-		USART_OUT(UART4,(uint8_t*)"%d\t",(int)u1);
-		if(nowAngle>91)
-			startState=1;
-		if(startState==1&&fabs(lastX-GetX())<2&&fabs(lastY-GetY())<2)
+		USART_OUT(UART4,(uint8_t*)"%d\t",(int)u2);
+		USART_OUT(UART4,(uint8_t*)"%d\r\n",(int)u1);
+		switch(switchState)
 		{
-			errState=1;
-		}
-		USART_OUT(UART4,(uint8_t*)"%d\r\n",(int)errState);
-	    lastX=(int)GetX();
-		lastY=(int)GetY();
-		if(R>=2250)
-			state=0;
-		if(R<=250)
-		state=1;
-		if(state==0)
-			if(GetX()<10&&GetX()>3)
-				R-=150;
-		if(state==1)
-			if(GetX()<=10&&GetX()>=3)
-				R+=150;
-		uPlace();
-		uAng();
-		pulse=4096*speed/(WHEEL_DIAMETER*PI);
-		buff1=(1+0.5*WHEEL_TREAD/R)*pulse+u1;//左轮       u+右， u-左，
-		buff2=(1-0.5*WHEEL_TREAD/R)*pulse-u1;//右轮
-	   if(errState==0)
-	   {
-		   VelCrl(CAN2,1,buff2);
-		   VelCrl(CAN2,2,-buff1);
-	   }
-	   if(errState>=1)
-	   {
-		   VelCrl(CAN2,1,-10000);
-		   VelCrl(CAN2,2,10000);
-		   errState++;
-		   if(errState>=10)
+		 case 0:
+				if(distance2<=100)
+				{
+					motorState=0;
+					switchState=1;
+				}
+			    else if(distance1<=100)
+				{
+					motorState=1;
+					switchState=1;
+				}
+		 break;
+		 case 1:
+			 if(nowAngle>95||nowAngle<85)
+			  startState=1;
+			if(startState==1&&fabs(lastX-GetX())<2&&fabs(lastY-GetY())<2)
+			{
+				errCount++;
+				if(errCount>=60)
+				{
+					errState=1;
+					errCount=0;
+				}
+			}
+			lastX=(int)GetX();
+			lastY=(int)GetY();
+			if(R>=2000)
+				rState=0;
+			if(R<=500)
+				rState=1;
+			if(rState==0)
+			{
+				if(nowAngle>=170&&nowAngle<=180&&GetX()<=10&&GetX()>=2)
+					R-=250;
+			}
+			if(rState==1)
+			{
+				if(nowAngle>=170&&nowAngle<=180&&GetX()<=10&&GetX()>=2)
+					R+=250;
+			}
+			uPlace();
+			uAng();
+		   if(errState==0)
 		   {
-			   errState=0;
-			   R-=150;
+			   pulse=4096*speed/(WHEEL_DIAMETER*PI);
+			   buff1=(1+0.5*WHEEL_TREAD/R)*pulse+u1;//左轮       u+右， u-左，
+			   buff2=(1-0.5*WHEEL_TREAD/R)*pulse-u1;//右轮
+			   VelCrl(CAN2,1,buff2);
+			   VelCrl(CAN2,2,-buff1);
 		   }
-	   }
-//		switch(count)
-//		{
-//		 case 0:
-//		    State1=0;
-//		    State3=1;
-//		    WalkStright();
-//		    uPlace(1,0,1000,0);
-//	        uAng(u2,90);
-//		 if(GetY()>800.0/speed*1700)
-//			 count=1;
-//		 break;
-//		 case 1:
-//		    State1=0;
-//		    State3=0;
-//		    WalkStright();
-//		    uPlace(0,1000,2000,0);
-//	        uAng(u2,0);
-//		 if(GetX()>800.0/speed*1700)
-//			 count=2;
-//		 break;
-//		 case 2:
-//            State1=1;
-//		    State3=1;
-//		    WalkStright();
-//		    uPlace(0,2000,1000,2000);
-//	        uAng(u2,90);
-//		 if(GetY()<2000-800.0/speed*1700)
-//			 count=3;
-//		 break;
-//		 case 3:
-//            State1=1;
-//		    State3=0;
-//		    WalkStright();
-//		    uPlace(0,1000,0,0);
-//	        uAng(u2,0);
-//		 if(GetX()<2000-800.0/speed*1700)
-//			 count=0;
-//		 break;
-//       if(State3==1)
-//       {
-//		 uPlace(1,0,100);
-//	     uAng(u2,90);
-//	   }
-//	   if(State3==0)
-//       {
-//		 uPlace(0,100,0);
-//		 uAng(u2,atan(0)*180/PI+State2*180);
-//	   }
-//	    WalkStright(500);
-//	}
-  }		
+		   if(errState>=1)
+		   {
+			   VelCrl(CAN2,1,-13000);
+			   VelCrl(CAN2,2,13000);
+			   errState++;
+			   buffR=R;
+			   if(errState>=100)
+			   {
+				   errState=0;
+				   buffstate=1;
+				   if(R<=1400)
+					 R+=900;
+				   else
+					 R-=900;	   
+			   }
+		   }
+		   if(buffstate>=1)
+		  {
+			   buffstate++;
+			   if(buffstate>=180)
+			   {
+				   buffstate=0;
+				   R=buffR;
+			   }
+		   } 
+      break;		  
+	 }
+   }		
 }
 
